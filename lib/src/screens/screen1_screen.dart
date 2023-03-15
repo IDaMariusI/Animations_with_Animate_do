e01df87bb91a2e977c9bd7b883d73d4db7fe2f5b
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-15 09:36:54 -0600

    119. Inicio de proyecto - Animate_do_App

---
+++ lib/src/screens/screen1_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Screen1Screen extends StatelessWidget {
+   
+  const Screen1Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('Screen1Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:animate_do_app/src/screens/screen1_screen.dart';
