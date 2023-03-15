fa2c156259c520669afb9d6ae49748e2df60903b
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-15 10:41:20 -0600

    121. Animaciones basicas - Animate_do

--- lib/src/screens/screen1_screen.dart
+++ lib/src/screens/screen1_screen.dart
@@ -1,47 +1,80 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Screen1Screen extends StatelessWidget {
-  const Screen1Screen({Key? key}) : super(key: key);
+  const Screen1Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animate_do'),
+        title: FadeIn(
+          duration: const Duration(milliseconds: 500),
+          child: const Text('Animate_do'),
+        ),
         actions: <Widget>[
           IconButton(
             icon: const FaIcon(FontAwesomeIcons.twitter),
             onPressed: () {},
           ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            onPressed: () {},
+          SlideInLeft(
+            from: 100,
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (BuildContext context) => const Screen1Screen(),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const FaIcon(FontAwesomeIcons.play),
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const FaIcon(FontAwesomeIcons.play),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Icon(Icons.new_releases, color: Colors.blue, size: 40),
-            const Text(
-              'Title',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
+            ElasticIn(
+              delay: const Duration(milliseconds: 1100),
+              child: const Icon(
+                Icons.new_releases,
+                color: Colors.blue,
+                size: 40,
+              ),
             ),
-            const Text(
-              "I'm a small text",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+            FadeInDown(
+              delay: const Duration(milliseconds: 200),
+              child: const Text(
+                'Title',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
+              ),
             ),
-            Container(
-              color: Colors.blue,
-              height: 2,
-              width: 200,
+            FadeInDown(
+              delay: const Duration(milliseconds: 800),
+              child: const Text(
+                "I'm a small text",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+            ),
+            FadeInLeft(
+              delay: const Duration(milliseconds: 1100),
+              child: Container(
+                color: Colors.blue,
+                height: 2,
+                width: 200,
+              ),
             ),
           ],
         ),
